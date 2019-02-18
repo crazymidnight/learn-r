cb393c3fdb4d85741dc0e36f391be5f59b6881c8
Alexey Voytsekhovskiy <alexvoit70@gmail.com>
2019-02-18 11:53:46 +0700

    add exercises 1.1 and 1.2

---
+++ ex_1_1.R
@@ -0,0 +1,34 @@
+# A
+a <- 2.3
+
+res <- ((6 * a + 42) / (3 ^ (4.2 - 3.62)))
+
+print(res)
+
+# B
+i <- (-4) ^ 2 + 2
+ii <- -4 ^ 2 + 2
+iii <- (-4) ^ (2 + 2)
+iiii <- -4 ^ (2 + 2)
+
+print(i)
+print(ii)
+print(iii)
+print(iiii)
+
+# C
+nums <- c(25.2, 15, 16.44, 15.3, 18.6)
+
+resC <- (mean(nums) / 2) ^ 0.5
+
+print(resC)
+
+# D
+resD = log(0.3)
+
+print(resD)
+
+# E
+resE <- exp(resD)
+
+print(resE)
---
+++ ex_1_2.R
@@ -0,0 +1,23 @@
+# A
+seqA <- seq(from=5, to=-11, by=-0.3)
+print(seqA)
+
+# B
+seqA <- rev(seqA)
+
+print("Reversed seqA:")
+print(seqA)
+
+# C
+seqC <- rep(x=c(-1, 3, -5, 7, -9), each=10)
+
+print(sort(seqC, decreasing=TRUE))
+
+# D
+resD <- c(seq(from=6, to=12), rep(5.3, times=3), -3, seq(from=102, to=length(seqA), length.out=9))
+
+print("Result:")
+print(resD)
+
+print("Length of resD:")
+print(length(resD))
